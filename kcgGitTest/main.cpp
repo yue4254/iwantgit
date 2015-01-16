bbcabcfbabf6fa1f2b265b936c529333b80d563e
KCG <rlackdrms425@gmail.com>
2015-01-16 16:28:27 +0900

    first commit

---
+++ kcgGitTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  kcgGitTest
+//
+//  Created by 김창근 on 2015. 1. 16..
+//  Copyright (c) 2015년 KCG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	
+	printf("Hello, git!");
+	
+    return 0;
+}
